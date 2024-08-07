47985a87dca100ce4a49bfad749d774416177044
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-07 16:06:27 +0100

    edit dans Technicien.java

--- src/main/java/com/mouad/IT_Support/model/Technicien.java
+++ src/main/java/com/mouad/IT_Support/model/Technicien.java
@@ -1,9 +1,15 @@
 package com.mouad.IT_Support.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 
 @Entity
 public class Technicien extends Personne{
-
+    @OneToMany(mappedBy = "technicien")
+    @JsonIgnore
+    private List<Ticket> tickets;
 }
