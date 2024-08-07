a917cc70a45856dff26cc9b20838bbf6d920f09d
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-07 14:51:22 +0100

    creation de EquipmentRepository

--- src/main/java/com/mouad/IT_Support/repository/EquipmentRepository.java
+++ src/main/java/com/mouad/IT_Support/repository/EquipmentRepository.java
@@ -1,4 +1,9 @@
 package com.mouad.IT_Support.repository;
 
-public interface EquipmentRepository {
+import com.mouad.IT_Support.model.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 }
