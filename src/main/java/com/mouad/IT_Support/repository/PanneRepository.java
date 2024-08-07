40a641c89bd2ffa706a1eed5b1d4eae64b1d7d42
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-07 14:56:26 +0100

    creation de PanneRepository.java

---
+++ src/main/java/com/mouad/IT_Support/repository/PanneRepository.java
@@ -0,0 +1,7 @@
+package com.mouad.IT_Support.repository;
+
+import com.mouad.IT_Support.model.Panne;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PanneRepository extends JpaRepository<Panne, Long> {
+}
