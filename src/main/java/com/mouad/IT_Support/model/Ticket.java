d6fc47e9979d51a2a3ecb364c410193d6393a564
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-07 17:47:45 +0100

    Edit dans Ticket.java

--- src/main/java/com/mouad/IT_Support/model/Ticket.java
+++ src/main/java/com/mouad/IT_Support/model/Ticket.java
@@ -7,8 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalDateTime;
+
 
 @Entity
 @Data
@@ -20,7 +20,7 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ticketId;
     private String description;
-    private LocalDate dateDeCreation;
+    private LocalDateTime dateDeCreation;
     @Enumerated(EnumType.STRING)
     private EtatTicket statut;
 
