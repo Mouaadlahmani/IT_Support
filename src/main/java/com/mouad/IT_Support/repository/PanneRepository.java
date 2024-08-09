f1331b7c9bd456343d47e71e01ab10a89f63fb82
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-09 11:20:07 +0100

    Edit dans UtilisateurRepository, TicketRepository, TechnicienRepository, PersonneRepository et PanneRepository

--- src/main/java/com/mouad/IT_Support/repository/PanneRepository.java
+++ src/main/java/com/mouad/IT_Support/repository/PanneRepository.java
@@ -2,6 +2,8 @@ package com.mouad.IT_Support.repository;
 
 import com.mouad.IT_Support.model.Panne;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PanneRepository extends JpaRepository<Panne, Long> {
 }
--- src/main/java/com/mouad/IT_Support/repository/TicketRepository.java
+++ src/main/java/com/mouad/IT_Support/repository/TicketRepository.java
@@ -4,9 +4,10 @@ import com.mouad.IT_Support.model.Technicien;
 import com.mouad.IT_Support.model.Ticket;
 import com.mouad.IT_Support.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findAllByUtilisateur(Utilisateur utilisateur);
     List<Ticket> findAllByTechnicien(Technicien technicien);
