3ebf6ad996b3dd2232c6389401c7583607636346
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-09 09:43:56 +0100

    Creation TicketRepository

--- src/main/java/com/mouad/IT_Support/repository/TicketRepository.java
+++ src/main/java/com/mouad/IT_Support/repository/TicketRepository.java
@@ -1,7 +1,13 @@
 package com.mouad.IT_Support.repository;
 
+import com.mouad.IT_Support.model.Technicien;
 import com.mouad.IT_Support.model.Ticket;
+import com.mouad.IT_Support.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findAllByUtilisateur(Utilisateur utilisateur);
+    List<Ticket> findAllByTechnicien(Technicien technicien);
 }
