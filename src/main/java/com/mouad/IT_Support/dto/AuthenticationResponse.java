7be6a288598cd73347fac347a3fb4ebfe5b58f16
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-09 09:42:13 +0100

    Creation AuthenticationResponse

---
+++ src/main/java/com/mouad/IT_Support/dto/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.mouad.IT_Support.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+}
