50944bea14ebceae2eeb81ac16dae2674caac9d1
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-09 09:43:17 +0100

    Creation TicketRepository.java

---
+++ src/main/java/com/mouad/IT_Support/repository/TechnicienRepository.java
@@ -0,0 +1,8 @@
+package com.mouad.IT_Support.repository;
+
+import com.mouad.IT_Support.model.Technicien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TechnicienRepository extends JpaRepository<Technicien, Long> {
+
+}
